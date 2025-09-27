34e78cb2ace1aefec1359615bfacc883b41d38f7
akashstwt <akashpatel115347@gmail.com>
2025-09-28 01:56:36 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import React from 'react'
-import Header from './components/Header'
-import ChatInterface from './components/ChatInterface'
-import Features from './components/Features'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Pricing from './components/Pricing'
-import ScrollFillText from './components/ScrollFillText'
+import Hero from "../components/";
 
-const page = () => {
-  return (
-      <main>
-        <Header />
-        <Hero />
-        <ScrollFillText />
-        <ChatInterface />
-        <Features />
-        <Pricing />
-        <Footer />
-      </main>
-  )
+export default async function Home() {
+	return( 			
+			<section>
+			<Hero/>
+			</section>
+	);
 }
-
-export default page
